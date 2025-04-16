fb931587f7390f5c9eab677b92a5726f377d2cfa
Mike Alhayek <mike@crestapps.com>
2025-04-16 07:44:27 -0700

    Add project files.

---
+++ AIToolCallingSample/ImportOrchardCoreRecipeTool.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Extensions.AI;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+
+namespace AIToolCallingSample;
+
+public sealed class ImportOrchardCoreRecipeTool : AIFunction
+{
+    public override string Name => "import_recipe";
+
+    public override string Description => "Imports a dynamic OrchardCore JSON recipe";
+
+    public override JsonElement JsonSchema { get; }
+
+    public ImportOrchardCoreRecipeTool()
+    {
+        var metadata = new JsonObject
+        {
+            {"type", "object"},
+            {"properties", new JsonObject
+                {
+                    { "recipe", new JsonObject
+                        {
+                            {"type", "object" },
+                            {"description", "A JSON object representing an OrchardCore recipe" },
+                        }
+                    }
+                }
+            },
+            {"required", new JsonArray("recipe") },
+            {"return_type", new JsonObject
+                {
+                    {"type", "boolean"},
+                    {"description", "The result of the import process. True representing a successful import while false failed."},
+                }
+            },
+        };
+
+        JsonSchema = JsonSerializer.Deserialize<JsonElement>(metadata);
+    }
+
+    protected override ValueTask<object?> InvokeCoreAsync(AIFunctionArguments arguments, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(arguments);
+
+        if (!arguments.TryGetValue("recipe", out var data))
+        {
+            return ValueTask.FromResult<object?>(false);
+        }
+
+        var recipe = ConvertToJsonObject(data!);
+
+        if (recipe == null)
+        {
+            return ValueTask.FromResult<object?>(false);
+        }
+
+        Console.WriteLine($"The {Name} function was invoked.");
+
+        return ValueTask.FromResult<object?>(true);
+    }
+
+    private static JsonObject? ConvertToJsonObject(object data)
+    {
+        JsonObject? recipe = null;
+
+        if (data is JsonObject obj)
+        {
+            recipe = obj;
+        }
+        else
+        {
+            string? json;
+
+            if (data is JsonElement jsonElement)
+            {
+                json = jsonElement.GetString();
+            }
+            else
+            {
+                json = data.ToString();
+            }
+
+            try
+            {
+                recipe = JsonSerializer.Deserialize<JsonObject>(json);
+            }
+            catch { }
+        }
+
+        return recipe;
+    }
+}
---
+++ AIToolCallingSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using AIToolCallingSample;
+using Azure.AI.OpenAI;
+using Microsoft.Extensions.AI;
+using Microsoft.Extensions.Configuration;
+using System.ClientModel;
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+
+string? model = config["ModelName"] ?? throw new InvalidOperationException("ModelName is not defined in the secrets");
+string key = config["OpenAIKey"] ?? throw new InvalidOperationException("OpenAIKey is not defined in the secrets");
+string endpoint = config["Endpoint"] ?? throw new InvalidOperationException("Endpoint is not defined in the secrets");
+
+// Create the IChatClient
+IChatClient chatClient = new AzureOpenAIClient(new Uri(endpoint), new ApiKeyCredential(key))
+    .GetChatClient(model ?? "gpt-4o")
+    .AsIChatClient();
+
+var chatHistory = new List<ChatMessage>();
+
+var options = new ChatOptions
+{
+    Tools = [new ImportOrchardCoreRecipeTool()]
+};
+
+while (true)
+{
+    Console.WriteLine("Your prompt:");
+    var userPrompt = Console.ReadLine();
+    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
+
+    Console.WriteLine("AI Response:");
+    var response = "";
+    await foreach (var item in chatClient.GetStreamingResponseAsync(chatHistory, options, CancellationToken.None))
+    {
+        Console.Write(item.Text);
+        response += item.Text;
+    }
+    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
+    Console.WriteLine();
+}
